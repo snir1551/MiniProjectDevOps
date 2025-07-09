cfe4223bbe3bda49f3097a5fa458d8b9ae527b68
snir1551 <58264273+snir1551@users.noreply.github.com>
2025-07-09 12:34:10 +0300

    Update backend.tf

--- Terraform/dev/backend.tf
+++ Terraform/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "dev-rg"
-    storage_account_name = "mtcstatetf"
+    storage_account_name = "mtcstatetfdev"
     container_name       = "tfstate-dev"
     key                  = "terraform.dev.tfstate"
   }
